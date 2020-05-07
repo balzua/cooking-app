36317a6fb19d5c3c5f0beb630c076e58dd4b4298
Brian Alzua <balzua@gmail.com>
2020-05-07 17:25:29 -0500

    initial express file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import express = require('express');
+const PORT_NUMBER: Number = 3000;
+const app: express.Application = express();
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(PORT_NUMBER, function () {
+    console.log(`App is now listening on port ${PORT_NUMBER}`);
+})
